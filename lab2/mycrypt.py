b7a24c5c8f95571ac581401a111125b795d09c8a
Hemeba47 <60132661+Hemeba47@users.noreply.github.com>
2021-03-14 21:57:40 +0200

    Update mycrypt.py

--- lab2/mycrypt.py
+++ lab2/mycrypt.py
@@ -5,9 +5,21 @@ def encode(s):
         raise TypeError
     origlen = len(s)
     crypted = ""
+
+    #lisää loppuun välilyöntejä
+    s = s.ljust(1000-origlen)
+
     digitmapping = dict(zip('1234567890!"#€%&/()=','!"#€%&/()=1234567890'))
     if len(s) > 1000:
         raise ValueError
+
+    #FAILED test_mycrypt.py::test_invalid_char[\xe5\xe4\xf6] - Failed: DID NOT RAISE <class 'ValueError'>
+    if ('å' or 'ä' or 'ö') in s:
+        raise ValueError
+    #FAILED test_mycrypt.py::test_invalid_char[+] - Failed: DID NOT RAISE <class 'ValueError'>
+    if '+' in s:
+        raise ValueError
+
     for c in s:
         if c.isalpha():
             if c.islower():
@@ -15,10 +27,27 @@ def encode(s):
             # Rot13 the character for maximum security
             crypted+=codecs.encode(c,'rot13')
         elif c in digitmapping:
-          crypted+=digitmapping[c]
+            crypted+=digitmapping[c]
+            #toimii muuten paitsi se kusee encryptauksen testauksen koska niissä on lopussa pisteitä
+    #crypted = crypted.ljust(1000-origlen,".")
 
+    #poista lopuksi välilyönnit
+    crypted = crypted.rstrip()
     return crypted
 
 def decode(s):
-    return s
-
+    #FAILED test_mycrypt.py::test_encode_decode[123] - assert '!"#' == '123'
+    decrypted = ""
+    digitmapping = dict(zip('1234567890!"#€%&/()=','!"#€%&/()=1234567890'))
+    if len(s) > 1000:
+        raise ValueError
+    for c in s:
+        if c.isalpha():
+            if c.isupper():
+                c=c.lower()
+            # Rot13 the character for maximum security
+            decrypted+=codecs.encode(c,'rot13')
+        elif c in digitmapping:
+            decrypted+=digitmapping[c]
+    #decrypted = decrypted.rstrip(".")
+    return decrypted
